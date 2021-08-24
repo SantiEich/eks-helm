203f3e1294fd531c7e2089b60f923fb5f9fbffe2
Santiago Hernandez <santieich@gmail.com>
2021-08-24 19:04:32 -0300

    fix2

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,8 +6,11 @@ aws_region="$1"
 aws_access_key_id="$2"
 aws_secret_access_key="$3"
 
+export aws_access_key_id="$aws_access_key_id"
+export aws_secret_access_key="$aws_secret_access_key"
 
-aws eks update-kubeconfig --name stg-strike-cluster --region $region
+
+aws eks update-kubeconfig --name stg-strike-cluster --region $aws_region
 
 
 sh -c "cat ~/.kube/config"
