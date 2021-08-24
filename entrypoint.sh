95f2855ef7ad1ebe7d7c99cd9afc57484dcd4e44
Santiago Hernandez <santieich@gmail.com>
2021-08-24 19:10:30 -0300

    add custom command

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,12 +5,12 @@ set -e
 aws_region="$1"
 aws_access_key_id="$2"
 aws_secret_access_key="$3"
+cluster_name="$4"
+command="$5"
 
 export aws_access_key_id="$aws_access_key_id"
 export aws_secret_access_key="$aws_secret_access_key"
 
+aws eks update-kubeconfig --name $cluster_name --region $aws_region
 
-aws eks update-kubeconfig --name stg-strike-cluster --region $aws_region
-
-
-sh -c "cat ~/.kube/config"
+sh -c "$command"
